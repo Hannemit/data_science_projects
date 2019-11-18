53551b375c2653853340f56ac8e006043fb37a6e
Hannemit <hannehoitzing@gmail.com>
2019-11-18 11:00:35 +0000

    Add extra transformation function, change single into double quote

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -11,73 +11,110 @@ import numpy as np
 import os
 from src.data import add_country_codes
 
-INPUT_FILE = './data/raw/who_suicide_statistics.csv'
-POPULATION_FILE = './data/raw/total_pop_1960_2018.csv'
-META_DATA = './data/raw/meta_data.csv'
-PROCESSED_FILE = './data/processed/enriched_df.csv'
-CHOROPLETH_DATA_FILE = './data/processed/choropleth_df.csv'
-CLEANED_META_DATA_FILE = './data/processed/cleaned_meta.csv'
+INPUT_FILE = "./data/raw/who_suicide_statistics.csv"
+POPULATION_FILE = "./data/raw/total_pop_1960_2018.csv"
+META_DATA = "./data/raw/meta_data.csv"
+PROCESSED_FILE = "./data/processed/enriched_df.csv"
+CHOROPLETH_DATA_FILE = "./data/processed/choropleth_df.csv"
+CLEANED_META_DATA_FILE = "./data/processed/cleaned_meta.csv"
+CONVENIENT_DATA_FILE = "./data/processed/year_country_data.csv"
 
 
 def add_age_group_fractions(data_frame):
-    assert 'year' in data_frame, 'year column must be in dataframe'
-    assert 'country' in data_frame, 'country column must be in dataframe'
+    assert "year" in data_frame, "year column must be in dataframe"
+    assert "country" in data_frame, "country column must be in dataframe"
 
     # get rid of all rows with nan values in the population column
-    data_frame = data_frame.dropna(subset=['population'], how='any', axis=0)
-    assert len(data_frame) % 12 == 0, f'length of data frame must be multiple of 12, not {len(data_frame)}'
+    data_frame = data_frame.dropna(subset=["population"], how="any", axis=0)
+    assert len(data_frame) % 12 == 0, f"length of data frame must be multiple of 12, not {len(data_frame)}"
 
     # add total population size column
-    total_pop = data_frame.groupby(['country', 'year'])['population'].sum().reset_index() \
-        .rename(columns={'population': 'total_population'})
-    assert min(total_pop['total_population']) > 0, 'need valid total population count'
-    df = pd.merge(data_frame, total_pop, how='left', on=['country', 'year'])
+    total_pop = data_frame.groupby(["country", "year"])["population"].sum().reset_index() \
+        .rename(columns={"population": "total_population"})
+    assert min(total_pop["total_population"]) > 0, "need valid total population count"
+    df = pd.merge(data_frame, total_pop, how="left", on=["country", "year"])
 
     # add column for relative pop size of each age group
-    df['fraction_pop'] = df['population'] / df['total_population']
-    assert ((df['fraction_pop'] >= 0) & df['fraction_pop'] <= 1).sum() == len(df)
+    df["fraction_pop"] = df["population"] / df["total_population"]
+    assert ((df["fraction_pop"] >= 0) & df["fraction_pop"] <= 1).sum() == len(df)
 
     return df
 
 
 def get_age_group_stats(data_frame):
-    assert 'fraction_pop' in data_frame, 'dataframe needs to have column fraction_pop'
-    assert data_frame['fraction_pop'].isna().sum() == 0, 'there are nans in dataframe..'
+    assert "fraction_pop" in data_frame, "dataframe needs to have column fraction_pop"
+    assert data_frame["fraction_pop"].isna().sum() == 0, "there are nans in dataframe.."
 
-    grouped = data_frame.groupby(['age', 'sex'])['fraction_pop'].mean().reset_index()
-    total_fraction = grouped['fraction_pop'].sum()
+    grouped = data_frame.groupby(["age", "sex"])["fraction_pop"].mean().reset_index()
+    total_fraction = grouped["fraction_pop"].sum()
     assert 1.01 >= total_fraction >= 0.99  # check whether overall fraction roughly 1
 
     # normalize to exactly 1.0 overall fraction
-    grouped['fraction_pop'] = grouped['fraction_pop'] / grouped['fraction_pop'].sum()
+    grouped["fraction_pop"] = grouped["fraction_pop"] / grouped["fraction_pop"].sum()
 
     return grouped
 
 
 def clean_population_stats(data_frame):
-    df_pop = data_frame.drop(['Country Code', 'Indicator Name', 'Indicator Code', 'Unnamed: 63'], axis=1)
-    df_pop = df_pop.rename(columns={'Country Name': 'country'})
-    df_pop = df_pop.melt(id_vars=["country"], var_name="year", value_name='total_population')
+    df_pop = data_frame.drop(["Country Code", "Indicator Name", "Indicator Code", "Unnamed: 63"], axis=1)
+    df_pop = df_pop.rename(columns={"Country Name": "country"})
+    df_pop = df_pop.melt(id_vars=["country"], var_name="year", value_name="total_population")
 
-    df_pop['year'] = df_pop['year'].astype('float')
+    df_pop["year"] = df_pop["year"].astype("float")
 
     return df_pop
 
 
 def fill_in_missing_populations(data_frame, df_population, df_age_statistics):
-    added_age_stats = pd.merge(data_frame, df_age_statistics, how='left', on=['sex', 'age'])
-    added_total_pop = pd.merge(added_age_stats, df_population, how='left', on=['country', 'year'])
+    added_age_stats = pd.merge(data_frame, df_age_statistics, how="left", on=["sex", "age"])
+    added_total_pop = pd.merge(added_age_stats, df_population, how="left", on=["country", "year"])
 
     # now replace any missing values in population with the product of total population and fraction in age group
-    mask = added_total_pop['population'].isnull()
+    mask = added_total_pop["population"].isnull()
 
-    added_total_pop.loc[mask, 'population'] = \
-        added_total_pop['fraction_pop'][mask] * added_total_pop['total_population'][mask]
+    added_total_pop.loc[mask, "population"] = \
+        added_total_pop["fraction_pop"][mask] * added_total_pop["total_population"][mask]
 
     # drop total population column and fraction_pop columns
-    added_total_pop.drop(columns=['fraction_pop', 'total_population'], inplace=True)
+    added_total_pop.drop(columns=["fraction_pop", "total_population"], inplace=True)
 
     return added_total_pop
+
+
+def make_df_nicer_format(data_frame):
+    """
+    Here we take in the dataframe that comes out "prepare_data_for_choropleth" and transform it so that we have
+    one row for every unique (country, year) combination, and columns that gives us the male suicide number and rate,
+    the female suicide number and rate, and the combined suicide rate
+    :param data_frame: pd Dataframe
+    :return: pd Dataframe, restructured.
+    """
+    df = data_frame.copy()
+
+    # create a column with no duplicates, so that we can use df.pivot with this column as index.
+    df["year_country"] = df["year"].map(str) + "_" + df["country"]
+
+    suicide_rate_by_sex = df.pivot(index="year_country", columns="sex", values="suicides per 100,000").reset_index()
+    population_by_sex = df.pivot(index="year_country", columns="sex", values="population").reset_index()
+    suicide_num_by_sex = df.pivot(index="year_country", columns="sex", values="suicides_no").reset_index()
+
+    combined = pd.merge(suicide_rate_by_sex, population_by_sex, on="year_country", suffixes=("_rate", "_pop"))
+    combined = pd.merge(combined, suicide_num_by_sex, on="year_country")
+    combined = combined.rename(columns={"female": "suicide_num_f", "male": "suicide_num_m"})
+
+    # get year and country columns back
+    combined["split"] = combined["year_country"].str.split('_')
+    combined[['year', 'country']] = pd.DataFrame(combined.split.values.tolist())
+
+    combined = combined.drop(columns=["year_country", "split"], axis=1)
+    del combined.index.name
+    combined.loc[combined["female_pop"] == 0, "female_pop"] = np.nan
+    combined.loc[combined["male_pop"] == 0, "male_pop"] = np.nan
+
+    combined["overall_rate"] = (combined["suicide_num_f"] + combined["suicide_num_m"]) / (
+                combined["female_pop"] + combined["male_pop"]) * 100000
+
+    return combined
 
 
 def prepare_data_for_choropleth(enriched_df):
@@ -91,15 +128,15 @@ def prepare_data_for_choropleth(enriched_df):
     df_suicides = enriched_df.copy()
 
     # total number of suicides per year per sex per country (so summing over different age groups)
-    df_suicides = df_suicides.groupby(['year', 'sex', 'country']).sum().reset_index()
+    df_suicides = df_suicides.groupby(["year", "sex", "country"]).sum().reset_index()
     df_suicides.loc[df_suicides["population"] == 0, "population"] = np.nan  # otherwise get inf for suicide rate
-    df_suicides['suicides per 100,000'] = df_suicides['suicides_no'] / df_suicides['population'] * 100000
+    df_suicides["suicides per 100,000"] = df_suicides["suicides_no"] / df_suicides["population"] * 100000
 
     # little data in 2015 and 2016, drop them for plotting purposes
     df_suicides = df_suicides[df_suicides["year"] < 2015]
 
     # for choropleth hover option, better to use scientific notation / few decimals
-    df_suicides["suicides per 100,000"] = df_suicides['suicides per 100,000'].apply(lambda x: "%.2f" % x).astype(float)
+    df_suicides["suicides per 100,000"] = df_suicides["suicides per 100,000"].apply(lambda x: "%.2f" % x).astype(float)
 
     # add alpha-3 country codes for choropleth
     df_suicides["code"] = df_suicides["country"].apply(add_country_codes.get_alpha3_code)
@@ -108,7 +145,7 @@ def prepare_data_for_choropleth(enriched_df):
 
 
 def clean_meta_data(meta_data_frame):
-    meta = meta_data_frame.loc[:, ~meta_data_frame.columns.str.contains('^Unnamed')]
+    meta = meta_data_frame.loc[:, ~meta_data_frame.columns.str.contains("^Unnamed")]
     meta = meta.rename(columns={"Country Code": "code", "Region": "region", "IncomeGroup": "income"})
     meta = meta.drop(columns=["SpecialNotes", "TableName"])
     return meta
@@ -121,36 +158,40 @@ def main():
     """
     print(os.getcwd())
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("making final data set from raw data")
 
-    logger.info('reading in data...')
+    logger.info("reading in data...")
     df = pd.read_csv(INPUT_FILE)
     df_population = pd.read_csv(POPULATION_FILE, skiprows=4)
     meta_data = pd.read_csv(META_DATA)
 
-    logger.info('processing data...')
+    logger.info("processing data...")
     age_stats = get_age_group_stats(add_age_group_fractions(df))
     df_pop = clean_population_stats(df_population)
     enriched_df = fill_in_missing_populations(df, df_pop, age_stats)
     meta = clean_meta_data(meta_data)
 
-    logger.info(f'saving enriched dataframe to {PROCESSED_FILE}')
+    logger.info(f"saving enriched dataframe to {PROCESSED_FILE}")
     enriched_df.to_csv(PROCESSED_FILE, index=False)
-    logger.info('saved enriched dataframe')
+    logger.info("saved enriched dataframe")
 
-    logger.info(f'saving cleaned meta data to {CLEANED_META_DATA_FILE}')
+    logger.info(f"saving cleaned meta data to {CLEANED_META_DATA_FILE}")
     meta.to_csv(CLEANED_META_DATA_FILE, index=False)
-    logger.info('saved cleaned meta data')
-
+    logger.info("saved cleaned meta data")
 
     logger.info(f"Creating data for choropleth plot, saving to {CHOROPLETH_DATA_FILE}")
     choropleth_df = prepare_data_for_choropleth(enriched_df)
     choropleth_df.to_csv(CHOROPLETH_DATA_FILE, index=False)
-    logger.info('saved choropleth dataframe')
+    logger.info("saved choropleth dataframe")
+
+    logger.info(f"Creating another dataframe in convenient format, saving to {CONVENIENT_DATA_FILE}")
+    year_country_df = make_df_nicer_format(choropleth_df)
+    year_country_df.to_csv(CONVENIENT_DATA_FILE, index=False)
+    logger.info("saved more convenient dataframe")
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
     logging.basicConfig(level=logging.INFO, format=log_fmt)
 
     # not used in this stub but often useful for finding various files
