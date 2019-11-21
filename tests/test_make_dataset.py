78229ba5ed5b3eeb6dccc18103e847c9688e6f32
Hannemit <hannehoitzing@gmail.com>
2019-11-21 12:11:33 +0000

    Add test

---
+++ tests/test_make_dataset.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from src.data.make_dataset import CHOROPLETH_DATA_FILE, CONVENIENT_DATA_FILE
+
+
+def test_make_df_nicer_format():
+    """
+    Perform some checks on whether we didn't change anything weird in making the more convenient dataframe to work with
+    It should contain the same information as the choropleth dataframe, just differently structured. We're here just
+    checking that they're similar
+    """
+
+    try:
+        df_choropleth = pd.read_csv(CHOROPLETH_DATA_FILE)
+        df_convenient = pd.read_csv(CONVENIENT_DATA_FILE)
+    except FileNotFoundError:
+        raise FileNotFoundError("Before running this test, run make_dataset.py to create our processed datafiles")
+
+    both = df_choropleth.groupby(['year', 'country']).agg(population=pd.NamedAgg(column="population", aggfunc=sum),
+                                                          suicides_no=pd.NamedAgg(column="suicides_no", aggfunc=sum),
+                                                          code=pd.NamedAgg(column="code", aggfunc=lambda x: x[0]),
+                                                          ).reset_index()
+    both["overall_rate"] = both['suicides_no'] / both['population'] * 100000
+    right = df_convenient[["year", "country", "code", "population", "overall_rate", "suicides_no"]]
+    pd.testing.assert_frame_equal(both.sort_index(axis=1), right.sort_index(axis=1))  # sort_index to get same col order
+
+    left = df_choropleth.loc[df_choropleth["sex"] == "female"].drop(columns=["sex"], axis=1)
+    right = df_convenient[["year", "country", "code", "female_pop", "female_rate", "suicide_num_f"]]
+    right.index = list(range(len(right)))
+    left.index = list(range(len(left)))
+    right = right.rename(columns={"female_rate": "suicides per 100,000",
+                                  "suicide_num_f": "suicides_no",
+                                  "female_pop": "population"})
+    pd.testing.assert_frame_equal(left.sort_index(axis=1), right.sort_index(axis=1))
+
+    left = df_choropleth.loc[df_choropleth["sex"] == "male"].drop(columns=["sex"], axis=1)
+    right = df_convenient[["year", "country", "code", "male_pop", "male_rate", "suicide_num_m"]]
+    right.index = list(range(len(right)))
+    left.index = list(range(len(left)))
+    right = right.rename(columns={"male_rate": "suicides per 100,000",
+                                  "suicide_num_m": "suicides_no",
+                                  "male_pop": "population"})
+    pd.testing.assert_frame_equal(left.sort_index(axis=1), right.sort_index(axis=1))
